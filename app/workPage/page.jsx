21e4c1a1e47d92fbd9efbc030a397af2dd231dbd
Ayush <ayushsharma1452@gmail.com>
2024-08-17 01:54:16 -0400

    added the work page

--- app/workPage/page.jsx
+++ app/workPage/page.jsx
@@ -3,10 +3,10 @@
 import { motion } from "framer-motion";
 import React, { useState } from "react";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+//import { Swiper, SwiperSlide } from "swiper/react";
+//import "swiper/css";
 
-import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+//import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 
 import {
     Tooltip,
@@ -17,7 +17,43 @@ import {
   
 import Link from "next/link";
 import Image from "next/image";
-import WorkSliderBtns from "@/components/WorkSliderBtns";
+//import WorkSliderBtns from "@/components/WorkSliderBtns";
+
+const projects = [
+    {
+      num: "01",
+      category: "frontend",
+      title: "project 1",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+      stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+      image: "/assets/work/thumb1.png",
+      live: "",
+      github: "",
+    },
+    {
+      num: "02",
+      category: "fullstack",
+      title: "project 2",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+      stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "Node.js" }],
+      image: "/assets/work/thumb2.png",
+      live: "",
+      github: "",
+    },
+    {
+      num: "03",
+      category: "frontend",
+      title: "project 3",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+      stack: [{ name: "Next.js" }, { name: "Tailwind.css" }],
+      image: "/assets/work/thumb3.png",
+      live: "",
+      github: "",
+    },
+  ];
 
 const Services = () => {
     return <div>work page</div>;
